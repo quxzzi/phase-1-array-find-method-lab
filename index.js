24b0c9fac830a84ca72a0d0e425c1bd0255a0a93
Kevin Dean <kdean1011@gmail.com>
2023-05-04 06:06:51 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record) {
+    const winRecord = record.filter(obj => obj.result === 'W');
+    if (winRecord.length > 0) {
+      return winRecord[0].year;
+    } else {
+      return undefined;
+    }
+  }
